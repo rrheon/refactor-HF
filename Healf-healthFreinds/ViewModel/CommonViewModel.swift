15722af3f5e9fb0b077fd24a6a6d494f863caa5d
yheon <mrdydgjs@inu.ac.kr>
2024-03-14 18:13:05 +0900

    feat - 작성하기 기능 구현 중

---
+++ Healf-healthFreinds/ViewModel/CommonViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CommonViewModel.swift
+//  Healf-healthFreinds
+//
+//  Created by 최용헌 on 3/14/24.
+//
+
+import Foundation
---
+++ Healf-healthFreinds/ViewModel/WriteHistoryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WriteHistoryViewModel.swift
+//  Healf-healthFreinds
+//
+//  Created by 최용헌 on 3/14/24.
+//
+
+import Foundation
