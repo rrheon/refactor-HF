d038d1d3df31cadff635b51c9b5a5b4bac92985a
yheon <mrdydgjs@inu.ac.kr>
2024-03-22 23:30:38 +0900

    feat - 마이페이지 값 세팅         게시글 작성, 기록 작성 시 count 증가         해당 값 마이페이지에 적용

--- Healf-healthFreinds/Model/ChatUserModel.swift
+++ Healf-healthFreinds/Model/ChatUserModel.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 // 나중에 프로필사진도 넣어야함
-class UserModel: NSObject {
+class ChatUserModel: NSObject {
   @objc var nickname: String?
   @objc var uid: String?
+  @objc var profileImage: String?
+  var togetherCount: Int?
+  var workoutCount: Int?
 }
+
+ 
