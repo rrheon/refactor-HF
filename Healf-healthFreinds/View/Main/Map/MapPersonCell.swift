904d9569aaefd3938e082d077ee45a37c5279063
yheon <mrdydgjs@inu.ac.kr>
2024-03-26 19:14:20 +0900

    feat - mapVC 수정 중

---
+++ Healf-healthFreinds/View/Main/Map/MapPersonCell.swift
@@ -0,0 +1,8 @@
+//
+//  MapPersonCell.swift
+//  Healf-healthFreinds
+//
+//  Created by 최용헌 on 3/26/24.
+//
+
+import Foundation
---
+++ Healf-healthFreinds/View/Main/Map/MapPersonListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MapPersonListViewController.swift
+//  Healf-healthFreinds
+//
+//  Created by 최용헌 on 3/26/24.
+//
+
+import Foundation
